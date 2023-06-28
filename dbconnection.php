1fa491fe2bc0f361d89b5fcd99d847c6515ac996
PhyuZinKyaw <phyuzinkyaw.cu.2028@gmail.com>
2023-06-28 22:56:05 +0630

    blogupate

---
+++ dbconnection.php
@@ -0,0 +1,13 @@
+<?php 
+    $server_name="localhost";
+    $dbname="blog_db";
+    $dbuser="root";
+    $dbpassword="";
+
+    $dsn="mysql::host=$server_name;dbname=$dbname";
+
+    $conn=new PDO($dsn,$dbuser,$dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    echo "connection success";
+?>
--- layouts/footer.php
+++ layouts/footer.php
@@ -1,3 +1,38 @@
+<?php 
+    $sql="SELECT * FROM categories";
+    $stmt=$conn->prepare($sql);
+    $stmt->execute();
+    $categories=$stmt->fetchall();
+
+?>        
+        
+        <!-- Side widgets-->
+        <div class="col-lg-4">
+                    <!-- Search widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Search</div>
+                        <div class="card-body">
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
+                                <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Categories widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Categories</div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <ul class="list-unstyled mb-0">
+                                        <?php foreach($categories as $category){?>
+                                        <li><a href="#!"><?= $category['name']?></a></li>
+                                        <?php }?>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
         <!-- Side widget-->
         <div class="card mb-4">
                         <div class="card-header">Side Widget</div>
